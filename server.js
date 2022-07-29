4023645c04d2aaee6d5c2143931974e2c69398aa
Samridha Das <samridhadas782001@gmail.com>
2022-07-29 16:54:24 +0530

    Mongo Setup

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
 const dbConnect = require('./dbConnect');
+const itemsRoute = require('./routes/itemsRoute');
+const cors = require('cors');
 
+app.use(cors());
+app.use(express.json());
+app.use('/api/items',itemsRoute);
 app.get('/',(req,res)=> res.send('Hello World'));
 app.listen(port,()=>console.log(`Listening on http://localhost:${port}`));
