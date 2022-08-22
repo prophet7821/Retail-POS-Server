f92fde6a87bd2b58b4b85fa5b4b48e960a01ca9a
Samridha Das <samridhadas782001@gmail.com>
2022-08-22 20:19:50 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port||5000;
+const port = process.env.PORT||5000;
 const dbConnect = require("./dbConnect");
 const itemsRoute = require("./routes/itemsRoute");
 const userRoute = require("./routes/userRoute");
